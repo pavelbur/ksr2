b9f8f0aa26f25c6248086ec5dea8b77806bbbd2f
pavelbur <buros11223344@gmail.com>
2015-07-05 22:49:27 +0300

    05-07-2015

--- index.php
+++ index.php
@@ -12,19 +12,30 @@
 </form>
 </html>
 <?php
-//var_dump($_GET['name']);
 function addInDb(){
     $name=$_GET['name'];
     $text=$_GET['text'];
     $DB = new PDO('mysql:host=localhost;dbname=gb', 'root', 'sten');
-
-    $DB->exec("INSERT INTO message (name,text) VALUES ($name,$text)");
-
-    $query="SELECT * FROM message";
+    $DB->exec("INSERT INTO message (name,text) VALUES ('$name','$text')");
+    header ("Location: index.php");
+    exit();
+}
+function show()
+{
+    $DB = new PDO('mysql:host=localhost;dbname=gb', 'root', 'sten');
+    $query = "SELECT * FROM message";
     $result = $DB->query($query, PDO::FETCH_ASSOC);
     foreach ($result as $row) {
-        echo $row['id'].' '.$row['name'].' '.$row['text'].'<br/>';
+        echo $row['id'] . ' ' . $row['name'] . ' ' . $row['text'] . '<br/>';
     }
 }
 
-addInDb();
+function formValidation(){
+    if(!empty($_GET['name'])){
+   addInDb();
+    } else
+        echo "Check form";
+
+}
+formValidation();
+show();
