30880f96645fd46c1b8bd8aef0c5eba908f70aea
pavelbur <buros11223344@gmail.com>
2015-07-05 16:02:30 +0300

    ver 3

--- index.php
+++ index.php
@@ -5,26 +5,17 @@
  * Date: 05.07.15
  * Time: 14:33
  */
-Class BD{
-    public $nameDB;
+Class DB{
+    protected $link;
     public function __construct($host,$login,$pass,$nameDB){
-        $this->nameDB=$nameDB;
-        mysql_connect($host,$login,$pass);
-    }
-    public function testConnection(){
-        $res=mysql_select_db($this->nameDB);
-        echo  ($res)?  "connect bd $this->nameDB" :  "connection failed";
-    }
-    public function addUser($userName){
-        mysql_query("INSERT INTO people (name) VALUES ('$userName')");
+      $this->link =  mysqli_connect($host,$login,$pass,$nameDB);
     }
     public function showUser(){
-        $row=mysql_query('SELECT * FROM people');
-        while($res = mysql_fetch_array($row)){
+       $row = mysqli_query($this->link ,'SELECT * FROM people');
+        while($res = mysqli_fetch_array($row)){
             echo $res['name'].'<br>';
         }
     }
 }
-$a= new BD('localhost','root','sten','test');
-$a->testConnection();
+$a = new DB('localhost','root','sten','test');
 $a->showUser();
