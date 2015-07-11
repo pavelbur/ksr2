aea3782d25dc879a5187fdb969ac786927ec162c
pavelbur <buros11223344@gmail.com>
2015-07-11 19:37:54 +0300

    function 2

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ function check(){
     if($_GET['page']){
         $x= abs((int)$_GET['page']);
     }
+    else{
+     $_GET['page']=1;
+    }
+
     if(is_integer($x)){
         return $x;
     }
@@ -37,13 +41,11 @@ function checkPage($numberPage,$page){
 }
 
 /**
- * @param boolean $data
- */
-/**
- * @param boolean $data
+ * @param bool $data
  * @param int $start
  * @param int $end
- * @param array $arr
+ * @param arr $arr
+ * @return array
  */
 function take($data,$start,$end,$arr){
     if($data){
@@ -52,14 +54,39 @@ function take($data,$start,$end,$arr){
     }
     else return array('1'=>'Error');
 }
+
+/**
+ * @return int|number возвращает с какой новости
+ */
 function startPage(){
 $x = (check()-1)*10;
     return $x;
 }
+
+/**
+ * @param $data номер стр из гета
+ * @param $start не придумал
+ * @param $curr
+ */
+function paginator(){
+   $x= count(createMsg());
+    $z=ceil($x/10);
+    $s='';
+    for($i=0;$i<$z;$i++){
+        $s.='<a href="index.php?page='.$i.'">'.$i.'</a>&nbsp;&nbsp;';
+    }
+    return $s;
+}
+
 function view(){
+
     $x = take(checkPage($_GET['page'],colPages(100)),startPage(),10,createMsg());
     foreach ($x as $item){
-        echo $item.'<br>';
+        echo $item.'<hr >';
+
     }
+
 }
+check();
 view();
+echo paginator();
