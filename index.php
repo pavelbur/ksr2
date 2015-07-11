c09da506d3a0e4eaa0b920843c41c8bdcdf73caf
pavelbur <buros11223344@gmail.com>
2015-07-11 17:10:15 +0300

    function

--- index.php
+++ index.php
@@ -1,41 +1,65 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>guest_book</title>
-</head>
-<form method="GET" action="index.php">
-    <input type="text" name="name">
-    <br>
-    <input type="text" name="text">
-    <br>
-    <input type="submit">
-</form>
-</html>
 <?php
-function addInDb(){
-    $name=$_GET['name'];
-    $text=$_GET['text'];
-    $DB = new PDO('mysql:host=localhost;dbname=gb', 'root', 'sten');
-    $DB->exec("INSERT INTO message (name,text) VALUES ('$name','$text')");
-    header ("Location: index.php");
-    exit();
+function check(){
+    if($_GET['page']){
+        $x= abs((int)$_GET['page']);
+    }
+    if(is_integer($x)){
+        return $x;
+    }
+    else{
+        return 1;
+    }
+
 }
-function show()
-{
-    $DB = new PDO('mysql:host=localhost;dbname=gb', 'root', 'sten');
-    $query = "SELECT * FROM message";
-    $result = $DB->query($query, PDO::FETCH_ASSOC);
-    foreach ($result as $row) {
-        echo $row['id'] . ' ' . $row['name'] . ' ' . $row['text'] . '<br/>';
+function createMsg(){
+    $x=array();
+    for($i=0;$i<100;$i++){
+        $x[]='Message: '.$i;
+    }
+    return $x;
+}
+function colPages($data){
+    $pages= ceil($data/10);
+    return $pages;
+}
+
+/**
+ * @param int $numberPage то что пришло из get
+ * @param int $page то что пришло из функции colPages
+ */
+function checkPage($numberPage,$page){
+    if($numberPage<=$page){
+        return true;
+    }
+    else{
+        return false;
     }
 }
 
-function formValidation(){
-    if(!empty($_GET['name'])){
-   addInDb();
-    } else
-        echo "Check form";
-
+/**
+ * @param boolean $data
+ */
+/**
+ * @param boolean $data
+ * @param int $start
+ * @param int $end
+ * @param array $arr
+ */
+function take($data,$start,$end,$arr){
+    if($data){
+        $x= array_slice($arr,$start,$end);
+        return $x;
+    }
+    else return array('1'=>'Error');
 }
-formValidation();
-show();
+function startPage(){
+$x = (check()-1)*10;
+    return $x;
+}
+function view(){
+    $x = take(checkPage($_GET['page'],colPages(100)),startPage(),10,createMsg());
+    foreach ($x as $item){
+        echo $item.'<br>';
+    }
+}
+view();
